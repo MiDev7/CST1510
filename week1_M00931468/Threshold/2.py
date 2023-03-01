51c2b7808867cb291c371fae7002a813f31f1bf6
MiDev7 <razafintsalamamikaia@gmail.com>
2023-03-01 23:08:29 +0400

    Threshold 2 and 3 added

---
+++ week1_M00931468/Threshold/2.py
@@ -0,0 +1,6 @@
+# Area and perimeter of a rectangle
+width = 4.5
+height = 7.9
+area = round((width * height),1)
+perimeter = round((2 * (width + height)),1)
+print(f'\t There is a reactangle of {width} of width with {height} of height. \n It\'s area is {area} and it\' perimeter is {perimeter}')
---
+++ week1_M00931468/Threshold/3.py
@@ -0,0 +1,4 @@
+# Convert pounds into kilograms
+weight_pd = float(input('Enter a value in pound: '))
+weight_kg = round((weight_pd * 0.454), 3)
+print(f'{weight_pd} pounds is {weight_pd} kilograms.')
