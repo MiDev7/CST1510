3c6d596ff06861acee0cf0ac86943c4aa64d8622
MiDev7 <razafintsalamamikaia@gmail.com>
2023-03-02 09:15:12 +0400

    Typical 4 is completed

---
+++ week2_M00931468/Typical/4.py
@@ -0,0 +1,21 @@
+# calculating the amount of money you will have in 3 years
+
+number_of_week_per_year = 52
+
+normalRate = 10
+
+yearOneRate = 20
+
+revenue_for_first_year = yearOneRate * number_of_week_per_year
+
+revenue_per_year = normalRate * number_of_week_per_year
+
+expense_per_year = 0.5 * (number_of_week_per_year / 2)
+
+sumFirstYearRevenue = revenue_for_first_year - expense_per_year
+
+sumTwoLastYear = (revenue_per_year - expense_per_year) * 2
+
+print(f'The amount of money after 3 year is: £{sumFirstYearRevenue + sumTwoLastYear}')
+
+
